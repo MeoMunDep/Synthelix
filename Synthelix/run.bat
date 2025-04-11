e23e2ebd5f6b46d3f934e8ba4ca889b95cc79006
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-12 03:24:47 +0700

    Update run.bat

--- Synthelix/run.bat
+++ Synthelix/run.bat
@@ -34,11 +34,11 @@ echo Checking dependencies...
 if exist "..\node_modules" (
     echo Using node_modules from parent directory...
     cd ..
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 tough-cookie axios-cookiejar-support
     cd %~dp0
 ) else (
     echo Installing dependencies in current directory...
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 tough-cookie axios-cookiejar-support
 )
 echo Dependencies installation completed!
 
